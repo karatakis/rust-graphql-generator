9218fefc82c6b9ca1521a5ac1820653e08fece77
Panagiotis Karatakis <panagiotiskaratakis@gmail.com>
2022-05-11 16:24:32 +0300

    Generate sql query filters

--- src/graphql_generator.rs
+++ src/graphql_generator.rs
@@ -4,7 +4,7 @@ use quote::{format_ident, quote};
 use sea_schema::sea_query::table::TableCreateStatement;
 use std::{collections::HashMap, fs};
 
-use crate::column_mapping::column_mapping;
+use sea_orm_codegen::Column;
 
 fn generate_graphql_entities(
     dir: &std::path::Path,
@@ -15,11 +15,49 @@ fn generate_graphql_entities(
         .map(|(name, table_meta)| {
             let table_name = format_ident!("{}", name.to_snake_case());
             let struct_name = format_ident!("{}", name.to_upper_camel_case());
+            let filter_name = format_ident!("{}Filter", name.to_upper_camel_case());
+
+            let mut filters: Vec<TokenStream> = Vec::new();
+            let mut filter_names: Vec<TokenStream> = Vec::new();
 
             let getters: Vec<_> = table_meta
+                .clone()
                 .get_columns()
                 .into_iter()
-                .map(|column| column_mapping(column))
+                .map(|column| {
+                    // let specs = column.get_column_spec();
+                    let column: Column = Column::from(column);
+
+                    let column_name = column.get_name_snake_case();
+                    let column_type = column.get_rs_type();
+
+                    let column_filter_name = format_ident!("{}{}Filter", struct_name, column.get_name_camel_case());
+
+                    filters.push(quote!{
+                        #[derive(async_graphql::InputObject, Debug)]
+                        struct #column_filter_name {
+                            equals: Option<#column_type>,
+                            not_equals: Option<#column_type>,
+                            greater_than: Option<#column_type>,
+                            greater_than_equals: Option<#column_type>,
+                            less_than: Option<#column_type>,
+                            less_than_equals: Option<#column_type>,
+                            r#in: Option<Vec<#column_type>>,
+                            not_in: Option<Vec<#column_type>>,
+                            is_null: Option<bool>,
+                        }
+                    });
+
+                    filter_names.push(quote!{
+                        #column_name: Option<#column_filter_name>
+                    });
+
+                    quote! {
+                        async fn #column_name(&self) -> &#column_type {
+                            &self.#column_name
+                        }
+                    }
+                })
                 .collect();
 
             quote! {
@@ -28,6 +66,15 @@ fn generate_graphql_entities(
                 #[async_graphql::Object]
                 impl #struct_name {
                     #(#getters)*
+                }
+
+                #(#filters)*
+
+                #[derive(async_graphql::InputObject, Debug)]
+                pub struct #filter_name {
+                    or: Option<Vec<Box<#filter_name>>>,
+                    and: Option<Vec<Box<#filter_name>>>,
+                    #(#filter_names),*
                 }
             }
         })
@@ -60,9 +107,11 @@ fn generate_root(dir: &std::path::Path, crate_stmts_map: HashMap<String, TableCr
         .map(|(name, _)| {
             let table_name = format_ident!("{}", name.to_snake_case());
             let struct_name = format_ident!("{}", name.to_upper_camel_case());
+            let filter_name = format_ident!("{}Filter", struct_name);
 
             quote! {
-                async fn #table_name<'a>(&self, ctx: &Context<'a>) -> Vec<#struct_name> {
+                async fn #table_name<'a>(&self, ctx: &Context<'a>, filters: Option<#filter_name>) -> Vec<#struct_name> {
+                    println!("filters: {:?}", filters);
                     use crate::orm::#table_name::Entity;
                     let db: &DatabaseConnection = ctx.data::<DatabaseConnection>().unwrap();
                     let data: Vec<#struct_name> = Entity::find().all(db).await.unwrap();
@@ -82,7 +131,7 @@ fn generate_root(dir: &std::path::Path, crate_stmts_map: HashMap<String, TableCr
     fs::write(dir.join("mod.rs"), mod_tokens.to_string()).unwrap();
 
     let query_root_tokens = quote! {
-        use super::entities;
+        use super::entities::*;
 
         #(#use_tokens)*
 

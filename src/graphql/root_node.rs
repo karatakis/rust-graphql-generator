faa77915b3303d643a37b2f7f1dfc015c6bd90e9
Panagiotis Karatakis <panagiotiskaratakis@gmail.com>
2022-06-09 12:29:50 +0300

    Add pagination

--- src/graphql/root_node.rs
+++ src/graphql/root_node.rs
@@ -2,10 +2,96 @@ use crate::types::{ColumnMeta, TableMeta};
 use proc_macro2::{TokenStream};
 use quote::{format_ident, quote};
 
-fn generate_recursive_filter_fn(table: &TableMeta) -> TokenStream {
-    let entity_module = format_ident!("{}", table.entity_module);
+pub fn generate_root(tables_meta: &Vec<TableMeta>) -> TokenStream {
+    let pagination_input = generate_pagination_input();
 
-    let columns_filters: Vec<TokenStream> = table
+    let paginated_result = generate_paginated_result(tables_meta);
+
+    let single_queries: Vec<TokenStream> = generate_single_queries(tables_meta);
+
+    quote! {
+        use super::entities;
+
+        use async_graphql::Context;
+        use sea_orm::prelude::*;
+
+        #pagination_input
+
+        #paginated_result
+
+        pub struct QueryRoot;
+
+        #[async_graphql::Object]
+        impl QueryRoot {
+            #(#single_queries)*
+        }
+    }
+}
+
+pub fn generate_single_queries(tables_meta: &Vec<TableMeta>) -> Vec<TokenStream> {
+    tables_meta
+        .iter()
+        .map(|table: &TableMeta| {
+            let entity_module = format_ident!("{}", table.entity_module);
+
+            let filter_recursive = generate_recursive_filter_fn(table);
+
+            quote! {
+                async fn #entity_module<'a>(
+                    &self, ctx: &Context<'a>,
+                    filters: Option<entities::#entity_module::Filter>,
+                    pagination: Option<PaginationInput>,
+                ) -> PaginatedResult<entities::#entity_module::Model> {
+                    println!("filters: {:?}", filters);
+
+                    #filter_recursive
+
+                    let db: &DatabaseConnection = ctx.data::<DatabaseConnection>().unwrap();
+
+                    let stmt = entities::#entity_module::Entity::find()
+                        .filter(filter_recursive(filters));
+
+                    if let Some(pagination) = pagination {
+                        let paginator = stmt
+                            .paginate(db, pagination.limit);
+
+                        let data: Vec<entities::#entity_module::Model> = paginator
+                            .fetch_page(pagination.page)
+                            .await
+                            .unwrap();
+
+                        let pages = paginator
+                            .num_pages()
+                            .await
+                            .unwrap();
+
+                        PaginatedResult {
+                            data,
+                            pages,
+                            current: pagination.page
+                        }
+                    } else {
+                        let data: Vec<entities::#entity_module::Model> = stmt
+                            .all(db)
+                            .await
+                            .unwrap();
+
+                        PaginatedResult {
+                            data,
+                            pages: 1,
+                            current: 1
+                        }
+                    }
+                }
+            }
+        })
+        .collect()
+}
+
+pub fn generate_recursive_filter_fn(table_meta: &TableMeta) -> TokenStream {
+    let entity_module = format_ident!("{}", table_meta.entity_module);
+
+    let columns_filters: Vec<TokenStream> = table_meta
         .columns
         .iter()
         .map(|column: &ColumnMeta| {
@@ -59,44 +145,35 @@ fn generate_recursive_filter_fn(table: &TableMeta) -> TokenStream {
     }
 }
 
-pub fn generate_root(tables_meta: &Vec<TableMeta>) -> TokenStream {
-    let single_queries: Vec<TokenStream> = tables_meta
+pub fn generate_pagination_input() -> TokenStream {
+    quote! {
+        #[derive(async_graphql::InputObject, Debug)]
+        pub struct PaginationInput {
+            pub limit: usize,
+            pub page: usize,
+        }
+    }
+}
+
+pub fn generate_paginated_result(tables_meta: &Vec<TableMeta>) -> TokenStream {
+    let derives: Vec<TokenStream> = tables_meta
         .iter()
-        .map(|table: &TableMeta| {
-            let entity_module = format_ident!("{}", table.entity_module);
-
-            let filter_recursive = generate_recursive_filter_fn(table);
-
-            quote! {
-                async fn #entity_module<'a>(&self, ctx: &Context<'a>, filters: Option<entities::#entity_module::Filter>) -> Vec<entities::#entity_module::Model> {
-                    println!("filters: {:?}", filters);
-
-                    #filter_recursive
-
-                    let db: &DatabaseConnection = ctx.data::<DatabaseConnection>().unwrap();
-
-                    let data: Vec<entities::#entity_module::Model> = entities::#entity_module::Entity::find()
-                        .filter(filter_recursive(filters))
-                        .all(db).await.unwrap();
-
-                    data
-                }
+        .map(|table_meta: &TableMeta| {
+            let name = format!("Paginated{}Result", table_meta.entity_name);
+            let module = format_ident!("{}", table_meta.entity_module);
+            quote!{
+                #[graphql(concrete(name = #name, params(entities::#module::Model)))]
             }
         })
         .collect();
 
     quote! {
-        use super::entities;
-
-        use async_graphql::Context;
-        use sea_orm::prelude::*;
-
-
-        pub struct QueryRoot;
-
-        #[async_graphql::Object]
-        impl QueryRoot {
-            #(#single_queries)*
+        #[derive(async_graphql::SimpleObject, Debug)]
+        #(#derives)*
+        pub struct PaginatedResult<T: async_graphql::ObjectType> {
+            pub data: Vec<T>,
+            pub pages: usize,
+            pub current: usize,
         }
     }
 }
